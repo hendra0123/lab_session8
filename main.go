5ae74b962c893a48c7222e30cf56332105afc62e
hendra0123 <nerstuff24@gmail.com>
2023-11-21 13:25:57 +0800

    commit1

--- main.go
+++ main.go
@@ -20,6 +20,6 @@ func main() {
 	quotient, remainder := mathops.Divide(10, 3)
 	fmt.Printf("Division: Quotient - %d, Remainder - %d\n", quotient, remainder)
 
-	result = mathops.Power(2, -5)
+	result = mathops.Power(2, 3)
 	fmt.Println("Power:", result)
 }
